64688bf2b6584eb024a5facad0c9d2bf999398e1
William Martinez <williamr1994@gmail.com>
2022-05-22 23:37:02 -0600

    connected dishes module to load and store data to db

--- main.c
+++ main.c
@@ -8,11 +8,11 @@
 #include "modules/bills.c"
 #include "modules/dishes.c"
 #include "lib/utils.h"
+#include "models/dish.h"
 
 int mainMenu()
 {
   system("cls");
-  // createTables();
   int option;
   printf("--------------------------------------------------\n          SISTEMA DE RESTAURANTE\n-------------------------------------------------- \n\n");
 
@@ -30,8 +30,9 @@ int mainMenu()
 
 int main()
 {
+  // createTables();
+  loadDishes();
   basicConfigurations();
-  initDishValues();
   int menuOption;
   while (menuOption != 4)
   {
