930a43cbcb8c30fa2442507dd904b24eeb2498a3
Carlos Recinos <carlosrecinos1999@gmail.com>
2022-06-13 07:50:20 -0600

    remaining dishes

--- modules/dishes.c
+++ modules/dishes.c
@@ -15,77 +15,93 @@ void saveDish(dish *newDish)
   int res;
   char sql[200];
 
-   res = sqlite3_open(DB_FILE, &db);
-   if (res)
-     {
-       printf("No se pudo abrir la base de datos: %s\n", sqlite3_errmsg(db));
-       exit(0);
-     }
+  res = sqlite3_open(DB_FILE, &db);
+  if (res)
+  {
+    printf("No se pudo abrir la base de datos: %s\n", sqlite3_errmsg(db));
+    exit(0);
+  }
 
   snprintf(sql, 200, "INSERT INTO dishes (id, name, price) VALUES (%d, '%s', %lf);", newDish->id, newDish->name, newDish->price);
 
   res = sqlite3_exec(db, sql, NULL, 0, &error);
-   if (res != SQLITE_OK)
-   {
-      printf("Error: %s\n", error);
-      sqlite3_free(error);
-   }
-   else
-   {
-      printf("Registro insertado!\n");
-   }
-}
-
-void saveDishes()
-{
-  for (int i = 0; i < 7; i++)
+  if (res != SQLITE_OK)
   {
-    saveDish(&dishes[i]);
+    printf("Error: %s\n", error);
+    sqlite3_free(error);
+  }
+  else
+  {
+    printf("Registro insertado!\n");
   }
 }
 
 dish readDish(int id)
 {
-    dish newDish;
-    printf("Ingrese el nombre del platillo %d: ", id);
-    readString(newDish.name, 20);
-    printf("Ingrese el precio del platillo %d: ", id);
-    scanf("%f", &newDish.price);
-    getchar();
-    newDish.id = id;
-    return newDish;
+  dish newDish;
+  printf("Ingrese el nombre del platillo %d: ", id);
+  readString(newDish.name, 20);
+  printf("Ingrese el precio del platillo %d: ", id);
+  scanf("%f", &newDish.price);
+  getchar();
+  newDish.id = id;
+  return newDish;
 }
 
 void readDishes()
 {
-    for (int i = 0; i < 7; i++)
+  int savedDishes = 0;
+  int lastId = 0;
+  printf("Platillos guardados actualmente: \n");
+  for (int i = 0; i < 7; i++)
+  {
+    dish *d = &dishes[i];
+    int *id = d->id;
+    if (id != NULL)
     {
-        dishes[i] = readDish(i+1);
+      lastId = d->id;
+      savedDishes += 1;
+      printf("%s\n", d->name);
     }
-    saveDishes();
+  }
+  waitUser();
+  printf("Nuevos platillos a guardar (actuales=%d): \n", savedDishes);
+  for (int i = 0; i < 7; i++)
+  {
+    dish *d = &dishes[i];
+    if (d->id == NULL)
+    {
+      lastId += 1;
+      savedDishes += 1;
+      printf("%s\n", d->name);
+      dishes[i] = readDish(lastId);
+      printf("Saved dish %s\n", dishes[i].name);
+      saveDish(&dishes[i]);
+    }
+  }
 }
 
 void printDishes()
 {
-    for (int i = 0; i < 7; i++)
-    {
-        printf("\n%d - %s - $%.2f", dishes[i].id, dishes[i].name, dishes[i].price);
-    }
+  for (int i = 0; i < 7; i++)
+  {
+    printf("\n%d - %s - $%.2f", dishes[i].id, dishes[i].name, dishes[i].price);
+  }
 }
 
-void loadDishes(){
+void loadDishes()
+{
   sqlite3 *db;
   char *error = 0;
   int res;
   char *sql;
   sqlite3_stmt *stmt;
-
-   res = sqlite3_open(DB_FILE, &db);
-   if (res)
-     {
-       printf("No se pudo abrir la base de datos: %s\n", sqlite3_errmsg(db));
-       exit(0);
-     }
+  res = sqlite3_open(DB_FILE, &db);
+  if (res)
+  {
+    printf("No se pudo abrir la base de datos: %s\n", sqlite3_errmsg(db));
+    exit(0);
+  }
 
   sql = "SELECT * FROM dishes;";
 
@@ -99,7 +115,8 @@ void loadDishes(){
   int i = 0;
   while (sqlite3_step(stmt) == SQLITE_ROW)
   {
-    if (i > 6){
+    if (i > 6)
+    {
       break;
     }
     dish d;
